e184451d8e10bc8ca72f78a6d98a43bb70ac4447
markovo4 <obf2007@gmail.com>
2024-02-13 16:17:19 +0200

    Homework-1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <ctime>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "\"To be \n           or not\n                           to be\"\n                                                \/Shakespeare\/" << std::endl;
     return 0;
+}
+
+
+int main(){
+    std::time_t now = time(0);
+    char* dt = ctime(&now);
+    std::cout << "The local date and time is: " << dt << std::endl;
+}
+
+int main(){
+    std::cout << "                                                        Volodymyr Kirillov" "\n                                                               *" "\n                                                               *""\n                                                               *""\n                                                          *         *"<< std::endl;
+    std::cout << "                                           Alex Kirillov              Lilia Kirillova" << std::endl;
+    std::cout << "                                                *                            *" "\n                                                *                            *""\n                                                *                            *""\n                                           *         *                  *         *"<< std::endl;
+    std::cout << "                                       Grandma     Grandpa          Grandma     Grandpa" << std::endl;
+}
+
+
+int main(){
+    std::cout << "         ************                    **********                ************                        *******"<< std::endl;
+    std::cout << "         ****        ***              ****         ****            ****        ***                   ***** *****" << std::endl;
+    std::cout << "         ****        ***            ****            ****           ****        ***                  *****   *****" << std::endl;
+    std::cout << "         ****        ***           ****              ****          ****        ***                 *****     *****" << std::endl;
+    std::cout << "         ************              ****              ****          ************                   *****       *****" << std::endl;
+    std::cout << "         ****        ***           ****              ****          ****        ***               ***** ******* *****" << std::endl;
+    std::cout << "         ****        ***           ****              ****          ****        ***              ***** ********* *****" << std::endl;
+    std::cout << "         ****        ***            ****            ****           ****        ***             *****             *****" << std::endl;
+    std::cout << "         ****        ***              ****        ****             ****        ***            *****               *****" << std::endl;
+    std::cout << "         ************                    **********                ************              *****                 *****" << std::endl;
+}
+
+
+
+int main(){
+    std::cout << "                            *" << std::endl;
+    std::cout << "              *" << std::endl;
+    std::cout << "                                      *" << std::endl;
+    std::cout << "                                              *                  *" << std::endl;
+    std::cout << "                                   \n\n\n\n                                                *                 *" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+
+}
+
+
+
+int main(){
+    std::cout << "              Welcome to my Resume" << std::endl;
+    std::cout << "          \|my                    Resume              \|" << std::endl;
+    std::cout << "          \|      is                                  \|" << std::endl;
+    std::cout << "          \|      on            Linked          in    \|" << std::endl;
 }
