3601db6f105540a1fabf36bb90b60d34e338db35
markovo4 <obf2007@gmail.com>
2024-02-22 15:29:11 +0200

    Homework-5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <cstdlib>
+using namespace std;
+// Program 1
+//int main() {
+//
+//    unsigned int numbers[5] = {1, 2, 3, 4, 5};
+//    int arrLength = sizeof(numbers)/sizeof(numbers[0]);
+//    for (int i = 0; i < arrLength; i++){
+//        cout << numbers[i] << endl;
+//    }
+//    cout << endl;
+//
+//    for (int i = arrLength; i >= 0; i--){
+//        cout << numbers[i] << endl;
+//    }
+//}
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+// Program 2
+//int main(){
+//    int randNumbers[20];
+//    for(int i = 0; i < 20; i++){
+//        randNumbers[i] = rand() % 100;
+//        if(randNumbers[i] % 2){
+//            cout << randNumbers[i] << " ";
+//        }
+//    }
+//}
+
+// Program 3
+
