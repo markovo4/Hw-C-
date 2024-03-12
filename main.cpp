e884a2f5115888c33c55c20f6d8cabddd9f1cbe9
markovo4 <obf2007@gmail.com>
2024-03-12 23:10:13 +0200

    Homework-14

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void pow(double* X, double* Y);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    double A;
+    double B;
+    pow(3, 3);
+    cin >> A;
+    cin >> B;
+    pow(&A, &B);
+    cout << "A: " << A;
+}
+
+void pow(double* X, double* Y) {
+    double result = 1.0;
+    for (int i = 0; i < *Y; i++) {
+        result *= *X;
+    }
+    *X = result;
 }
